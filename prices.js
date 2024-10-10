63dfbbec81fe12b741a16a742176981e69564cd9
eggsandchicken <161009157+eggsandchicken@users.noreply.github.com>
2024-10-10 21:12:35 +0530

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,12 +1,12 @@
 const prices = {
-    "Chicken Curry Cut (Skinless)": 12.00,
-    "Chicken Boneless Breast": 14.00,
-    "Chicken Steak (Raw)": 16.00,
-    "Chicken Soup Pieces (Frozen)": 9.00,
-    "Chicken Wings (Frozen - Skin On)": 7.00,
-    "Whole Chicken (Skin On - Frozen)": 15.00,
-    "Chicken Curry Cut with Skin (Frozen)": 11.00,
-    "Chicken Leg": 10.00,
-    "Chicken Mince": 13.00,
-    "Chicken Drumsticks (Skinless)": 9.00
+    "Chicken Curry Cut (Skinless)": 120.00,
+    "Chicken Boneless Breast": 240.00,
+    "Chicken Steak (Raw)": 100.00,
+    "Chicken Soup Pieces (Frozen)": 90.00,
+    "Chicken Wings (Frozen - Skin On)": 170.00,
+    "Whole Chicken (Skin On - Frozen)": 110.00,
+    "Chicken Curry Cut with Skin (Frozen)": 150.00,
+    "Chicken Leg": 30.00,
+    "Chicken Mince": 210.00,
+    "Chicken Drumsticks (Skinless)": 40.00
 };
